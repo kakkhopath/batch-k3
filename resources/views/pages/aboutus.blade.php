c0880cc2fd51e3a518fd406d8e1807762f66ff9f
Md Milton Chowdhury <miltonk875@gmail.com>
2024-12-02 19:35:24 +0600

    Removed all files from milton_chowdhury branch

--- resources/views/pages/aboutus.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-@section('title',$title)
-@section('content')
-<section>
-    <h1 style="text-align: center">Welcome To About Us</h1><br><br>
-    <p style="text-align: center">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-</section>
-@endsection
